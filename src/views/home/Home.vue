a7e6b9d492912070fcfa39feb898d31793563ce2
L-Q-H <7448691+qq962763883@user.noreply.gitee.com>
2020-09-21 17:21:39 +0800

    首页轮播图组件以及推荐组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <nav-bar class="nav-bar"><div slot="centre">购物街</div></nav-bar>
+  <div class="home">
+    <nav-bar class="nav-bar"><div slot="centre">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+
 import {getHomeMultidata} from 'network/home'
 
 export default {
  name: "Home",
  components: {
-  NavBar
+  NavBar,
+  HomeSwiper,
+  RecommendView
  },
  data() {
   return {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recommdens">
+    <div v-for="(item,index) in recommends" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div class="text">{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'RecommendView',
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []    
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.recommdens {
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  margin-top: 10px;
+  box-shadow: 0 10px 1px hsla(0, 1%, 37%, 0.102);
+  padding-bottom: 30px;
+}
+img {
+  width: 80px;
+  height: 80px;
+}
+.text {
+  font-size: 14px;
+  margin-top: 10px;
+}
+</style>
